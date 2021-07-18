b2a721186c6afce1438cabce0e0aa24a9d78e867
Edvard <edvbot@gmail.com>
2021-07-19 00:50:58 +0200

    added tag component

---
+++ components/Tag/Tag.jsx
@@ -0,0 +1,147 @@
+import PropTypes from 'prop-types';
+import { React } from 'react';
+// Components using this import must follow the VARIATIONS / SIZES pattern. 
+import { generateStyles } from '../../shared/variationsHelper';
+import CloseX from './assets/tag-close-x.svg';
+
+
+
+
+/** 
+ *  ######################################################
+ *  #            VARIATION and SIZE controls             #
+ *  ######################################################
+ */
+
+const COMMON = {
+  body: [ 'inline-block', 'select-none'],
+  text: [ 'font-effra', 'mx-1', 'overflow-hidden', 'flex', 'flex-grow' ],
+  count: ['ml-2', 'pl-1', 'border-l', 'inline', 'py-1'],
+  close: ['ml-2', 'inline-block', 'h-full', 'flex', 'items-center', 'cursor-pointer']
+}
+
+export const VARIATIONS = {
+  default: {
+    body: [ ...COMMON.body, 'bg-slate' ],
+    text: [ ...COMMON.text, 'text-white' ],
+    count: [ ...COMMON.count, 'border-charcoal' ],
+    close: [ ...COMMON.close ]
+  },
+  green: {
+    body: [ ...COMMON.body, 'bg-green' ],
+    text: [ ...COMMON.text, 'text-white' ],
+    count: [...COMMON.count, 'border-green-dark' ]
+  },
+  red: {
+    body: [ ...COMMON.body, 'bg-red' ],
+    text: [ ...COMMON.text, 'text-white' ],
+    count: [...COMMON.count, 'border-red-dark' ]
+  },
+  yellow: {
+    body: [ ...COMMON.body, 'bg-yellow' ],
+    text: [ ...COMMON.text, 'text-black' ]
+  },
+  yellowWhite: {
+    body: [ ...COMMON.body, 'bg-yellow' ],
+    text: [ ...COMMON.text, 'text-white' ]
+  },
+  blue: {
+    body: [ ...COMMON.body, 'bg-blue' ],
+    text: [ ...COMMON.text, 'text-white' ]
+  },
+  pink: {
+    body: [ ...COMMON.body, 'bg-pink' ],
+    text: [ ...COMMON.text, 'text-white' ],
+    count: [...COMMON.count, 'border-pink-dark' ]
+  },
+  lightBlue: {
+    body: [ ...COMMON.body, 'bg-blue-light' ],
+    text: [ ...COMMON.text, 'text-white' ]
+  },
+  lightGreen: {
+    body: [ ...COMMON.body, 'bg-green-light' ],
+    text: [ ...COMMON.text, 'text-white' ]
+  },
+  lightPink: {
+    body: [ ...COMMON.body, 'bg-pink-lighter' ],
+    text: [ ...COMMON.text, 'text-white' ]
+  },
+  orange: {
+    body: [ ...COMMON.body, 'bg-orange' ],
+    text: [ ...COMMON.text, 'text-white' ]
+  },
+  white: {
+    body: [ ...COMMON.body, 'bg-white', 'border', 'border-charcoal', 'dark:border-none' ],
+    text: [ ...COMMON.text, 'text-black' ]
+  },
+}
+
+export const SIZES = {
+  default: {
+    body: [ 'rounded-md' ],
+    text: [ 'text-sm' ],
+    close: [ 'w-4' ]
+  },
+  small: {
+    body: [ 'rounded-sm' ],
+    text: [ 'text-xs' ],
+    close: [ 'w-3' ]
+  },
+  large: {
+    body: [ 'rounded-lg' ],
+    text: [ 'text-xl' ],
+    close: [ 'w-5' ]
+  }
+}
+
+/** 
+ *  ######################################################
+ *  #                  Component logic                   #
+ *  ######################################################
+ */
+
+/**
+ * The default button which comes in a number of variations.
+ */
+export const Tag = ({ tagName, count, closeable, onClose, variation, size }) => {
+  const { 
+    body: bodyStyles, text: textStyles, count: countStyles, close: closeStyles
+  } = generateStyles(variation, size, VARIATIONS, SIZES);
+
+  return <div className={bodyStyles.join(' ')}>
+    <div className={textStyles.join(' ')}>
+      {tagName}
+      <div className='inline-block'>
+        {
+          // Render either the count (when given), or the close-button
+          count ? 
+            <span className={countStyles.join(' ')}>
+              {count}
+            </span> : null
+        }
+        {
+          closeable ? 
+            <div onClick={(event) => onClose(event)} className={closeStyles.join(' ')}>
+              <CloseX />
+            </div> : null
+        }
+      </div>
+    </div>
+  </div>
+};
+
+Tag.propTypes = {
+  count: PropTypes.number,
+  closeable: PropTypes.bool,
+  onClose: PropTypes.func,
+  size: PropTypes.oneOf(Object.keys(SIZES)),
+  variation: PropTypes.oneOf(Object.keys(VARIATIONS)),
+  tagName: PropTypes.string.isRequired,
+};
+
+Tag.defaultProps = {
+  variation: 'default',
+  size: 'default',
+  closeable: false,
+  onClose: undefined,
+};
---
+++ components/Tag/Tag.stories.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { SIZES, Tag, VARIATIONS } from './Tag';
+
+
+export default {
+  title: 'Indicators/Tag',
+  component: Tag,
+};
+
+const Template = (args) => <>
+  <p className='text-2xl my-3 dark:text-white'>Tags:</p>
+  <hr className='dark:border-white' />
+  {/* Spits out a grid of all combinations of VARIATIONS/SIZES */}
+  <div className='my-5 grid grid-cols-5 gap-4'>
+    {
+      Object.keys(SIZES)
+        .map(size => 
+          Object.keys(VARIATIONS).map(
+            variation => <div className={'col-span-1 flex justify-center'} key={variation}>
+              <div className={'flex-shrink'}>
+                <Tag 
+                  {...args}
+                  size={size} 
+                  variation={variation} 
+                />
+              </div>
+            </div>
+          ))
+    }
+  </div>
+</>
+
+export const Default = Template.bind({});
+Default.args = {
+  tagName: 'Resolved',
+};
+
+export const Closeable = Template.bind({});
+Closeable.args = {
+  tagName: 'Close me',
+  closeable: true,
+}
+
+export const Counter = Template.bind({});
+Counter.args = {
+  tagName: 'Count me',
+  count: 11
+}
